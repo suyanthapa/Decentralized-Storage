1899d7a3297e8374102b3758831ac172e9e66927
suyanthapa <146444078+suyanthapa@users.noreply.github.com>
2024-09-01 13:56:34 +0545

    Logo, icon Added

--- App1/src/components/Header.jsx
+++ App1/src/components/Header.jsx
@@ -1,42 +1,74 @@
 import React from 'react';
+import logo from "../assets/logo.png";
+import home from "../assets/home.png";
+import upload from "../assets/upload.png";
+import retrieve from "../assets/retrieve.png";
+import help from "../assets/help.png";
+import about from "../assets/about.png";
+
 
 const Header =({ connectWallet, disconnectWallet, account, setActiveSection, activeSection })=> {
   return (
     <div>
-      <aside className="fixed top-0 left-0 w-64 h-screen bg-gray-950 text-white pt-8 z-10 flex flex-col justify-between">
+      <aside className="fixed top-0 left-0 w-64 h-screen bg-gray-900 text-white pt-8 z-10 flex flex-col justify-between border border-black">
         <div>
-          <div className="text-white text-lg font-bold flex justify-center mb-8 ">DataBridge</div>
-          <nav className="flex flex-col items-center space-y-4">
+          <div className="flex flex-col items-center mb-8 ">
+            <img 
+            src={logo} 
+            alt=" Logo of the System" 
+            className='h-24 w-auto '
+             onClick={() => setActiveSection('home')}/>
+         
+        </div>
+          <nav className="flex  flex-col items-center space-y-4">
             <a
-               href="/" 
-               className={`hover:bg-gray-700 p-2 rounded w-full text-center ${activeSection === 'home' ? 'bg-gray-700' : 'hover:bg-gray-700' }`}
-                onClick={() => setActiveSection('home')}
+               href="#" 
+               className={`flex items-center  space-x-4 hover:bg-purple-950 pl-20 p-2 rounded w-full text-center `}
+               
             >
-                Home  
+              <img src={home} alt="Home Icon" className='h-5 w-auto '/>
+              <span>Dashboard</span>
             </a>
 
             <a
               href="#"
-              className={`hover:bg-gray-700 p-2 rounded w-full text-center ${activeSection === 'upload' ? 'bg-gray-700' : 'hover:bg-gray-700' }`}
+              className={`flex items-center space-x-4 hover:bg-purple-950 p-2 pl-20 rounded w-full text-center  ${activeSection === 'upload' ? 'bg-purple-900' : 'hover:bg-gray-700' }`}
               onClick={() => setActiveSection('upload')}
             >
-              Upload Files
+              <img src={upload} alt="Upload Icon" className='h-5 w-auto '/>
+              <span>Upload </span>
+            
             </a>
 
             <a
               href="#"
-              className={`hover:bg-gray-700 p-2 rounded w-full text-center ${activeSection === 'retrieve' ? 'bg-gray-700' : 'hover:bg-gray-700'}`}
+              className={`flex items-center space-x-4 hover:bg-purple-950 p-2  pl-20  rounded w-full text-center ${activeSection === 'retrieve' ? 'bg-purple-900' : 'hover:bg-gray-700'}`}
               onClick={() => setActiveSection('retrieve')}
             >
-              Retrieve Files
+              <img src={retrieve} alt="Retrieve Icon" className='h-5 w-auto '/>
+              <span>  Retreieve </span>
             </a>
 
-            <a href="/help" className="hover:bg-gray-700 p-2 rounded w-full text-center">Help</a>
-            <a href="/about" className="hover:bg-gray-700 p-2 rounded w-full text-center">About</a>
-            <a href="/contact" className="hover:bg-gray-700 p-2 rounded w-full text-center">Contact</a>
+            <a
+             href="/help" 
+             className=" flex items-center space-x-4 hover:bg-purple-950 p-2 pl-20 rounded w-full text-center"
+             >
+               <img src={help} alt="Help Icon" className='h-5 w-auto '/>
+               <span> Help</span>
+            </a>
+
+            <a
+             href="/about"
+             className="flex items-center space-x-4 hover:bg-purple-950 p-2 pl-20 rounded w-full text-center"
+             >
+             <img src={about} alt="About Icon" className='h-5 w-auto '/>
+             <span>About</span>
+             </a>
+
+           
           </nav>
           
-          <div className="text-white text-base flex justify-center mt-56  flex flex-col">
+          <div className="text-white text-base flex justify-center mt-44  flex flex-col">
             <div>
               <p className='flex justify-center'>
                 Address: {account ? `${account.slice(0,6)}.....${account.slice(-5)}` : "Not Connected"}
--- App1/src/components/Homepage.jsx
+++ App1/src/components/Homepage.jsx
@@ -4,19 +4,21 @@ import homeImage from '../assets/home.jpg'; // Correct import path
 const Homepage = () => {
   return (
     <div
-      className="relative text-white"
+      className="relative text-white bg-gray-800"
       style={{
-        backgroundImage: `url(${homeImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+  
+
+
+        // backgroundSize: 'cover',
+        // backgroundPosition: 'center',
         height: '779px',
       }}
     >
-      <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50">
-        <h1 className="text-6xl font-bold mb-5 text-fuchsia-300">Welcome to Data Chain</h1>
-        <p className="text-3xl font-bold mb-5 text-fuchsia-300">Secure Health Record Management</p>
+      <div className="flex flex-col justify-center items-center text-center h-screen">
+        <h1 className="text-6xl font-bold mb-5 "> <span className='text-green-400'>Unlock</span> the Power of Decentralized Storage with <span className='text-purple-400'>DataBridge</span></h1>
+        {/* <p className="text-3xl font-bold mb-5 text-fuchsia-300">Secure Health Record Management</p> */}
         <p className="text-xl mb-5 text-center max-w-xl">
-          Upload and access your health records securely on the blockchain with CryptoBridge. Ensure data integrity and privacy with Ethereum smart contracts.
+        Secure, Decentralized storage for all your important files.
         </p>
       </div>
     </div>
