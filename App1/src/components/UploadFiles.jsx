ba808ab946de7f16c4d45fb7b81e718c9c66e4fb
suyanthapa <146444078+suyanthapa@users.noreply.github.com>
2024-09-03 22:09:37 +0545

    drag and drog section for upload

--- App1/src/components/UploadFiles.jsx
+++ App1/src/components/UploadFiles.jsx
@@ -1,23 +1,31 @@
 import axios from "axios";
 import React, { useState } from 'react';
-import homeImage from '../assets/home.jpg';
+import { FaCloudUploadAlt } from 'react-icons/fa';
 
 const UploadFiles = ({ contractInstance, account, addHospitalName, addFileDetails }) => {
   const [hospital, setHospital] = useState('');
-  const [file, setFile] = useState(null);
+  const [files, setFiles] = useState([]);
   const [date, setDate] = useState(new Date().toISOString().split('T')[0]); // Default to current date
   const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(false); // Loading state
 
   const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-    if (selectedFile) {
-      setFile(selectedFile);
-    }
+    const selectedFiles = Array.from(e.target.files);
+    setFiles(prevFiles => [...prevFiles, ...selectedFiles]);
+  };
+
+  const handleFileDrop = (e) => {
+    e.preventDefault();
+    const droppedFiles = Array.from(e.dataTransfer.files);
+    setFiles(prevFiles => [...prevFiles, ...droppedFiles]);
+  };
+
+  const handleFileRemove = (fileToRemove) => {
+    setFiles(files.filter(file => file !== fileToRemove));
   };
 
   const handleUpload = async () => {
-    if (!account || !hospital || !file) {
+    if (!account || !hospital || files.length === 0) {
       setMessage('Please fill in all the fields.');
       return;
     }
@@ -25,104 +33,140 @@ const UploadFiles = ({ contractInstance, account, addHospitalName, addFileDetail
     setLoading(true); // Start loading
 
     try {
-      const formData = new FormData();
-      formData.append("file", file);
+      // Upload all files to IPFS and get their hashes
+      const fileHashes = [];
+      for (const file of files) {
+        const formData = new FormData();
+        formData.append("file", file);
 
-      const response = await axios({
-        method: "post",
-        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
-        data: formData,
-        headers: {
-          pinata_api_key: '0ce134a6f820a2671774',
-          pinata_secret_api_key: '1b7b42c773eb6a17c85bf82172a7925fc0934b08d25d2d77c84f7369eff420fe',
-          "Content-Type": "multipart/form-data",
-        },
-      });
+        const response = await axios({
+          method: "post",
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data: formData,
+          headers: {
+            pinata_api_key: '0ce134a6f820a2671774',
+            pinata_secret_api_key: '1b7b42c773eb6a17c85bf82172a7925fc0934b08d25d2d77c84f7369eff420fe',
+            "Content-Type": "multipart/form-data",
+          },
+        });
 
-      const fileHash = response.data.IpfsHash;
-      console.log(fileHash);
+        fileHashes.push(response.data.IpfsHash);
+      }
 
-      const tx = await contractInstance.uploadRecord(hospital, fileHash, date);
-      await tx.wait();
+      // Upload all records to the blockchain
+      for (const fileHash of fileHashes) {
+        const tx = await contractInstance.uploadRecord(hospital, fileHash, date);
+        await tx.wait();
+      }
 
-      // Successfully uploaded, now update Dashboard
-      setMessage('Record uploaded successfully!');
-      addFileDetails({
-        name: file.name,
-        type: file.type,
-        extension: file.name.split('.').pop(),
-        date: date
+      setMessage('Records uploaded successfully!');
+      files.forEach(file => {
+        addFileDetails({
+          name: file.name,
+          type: file.type,
+          extension: file.name.split('.').pop(),
+          date: date
+        });
       });
       addHospitalName(hospital);
     } catch (error) {
-      console.error("Error uploading record:", error);
-      setMessage('Error uploading record. See console for details.');
+      console.error("Error uploading records:", error);
+      setMessage('Error uploading records. See console for details.');
     } finally {
       setLoading(false); // End loading
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-900">
-      <div className="bg-gray-800 p-10 rounded-lg shadow-lg w-3/4 max-w-9xl flex">
-        <div className="w-full pr-5">
-          <h2 className="text-2xl font-bold text-center mb-5 text-white">Upload Health Records</h2>
-          <form className="space-y-5">
-            <div>
-              <label className="block text-white">Address:</label>
-              <input
-                type="text"
-                value={account}
-                readOnly
-                className="w-full p-2 border border-gray-600 rounded mt-1 bg-gray-700 text-white"
-              />
+    <div className="flex justify-center items-center h-screen bg-gray-100 text-black">
+      <div className="bg-gray-100 p-10 rounded-lg shadow-2xl w-3/4 max-w-9xl flex flex-col">
+        <h2 className="text-2xl font-bold text-center mb-5">Upload Health Records</h2>
+        <form className="space-y-5">
+          <div>
+            <label className="block">Address:</label>
+            <input
+              type="text"
+              value={account}
+              readOnly
+              className="w-full p-2 border border-gray-600 rounded mt-1 bg-gray-300"
+            />
+          </div>
+          <div>
+            <label className="block">Date:</label>
+            <input
+              type="date"
+              readOnly
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className="w-full p-2 border border-gray-600 rounded mt-1 bg-gray-300"
+            />
+          </div>
+          <div>
+            <label className="block">Hospital:</label>
+            <input
+              type="text"
+              value={hospital}
+              onChange={(e) => setHospital(e.target.value)}
+              placeholder="Enter hospital name"
+              className="w-full p-2 border border-gray-600 rounded mt-1 bg-gray-300"
+            />
+          </div>
+          <div> <label className="block">File:</label>
+         
+          <div
+            className="bg-white p-6 rounded-lg shadow-md mb-6 border-dashed border-2 border-gray-300"
+            onDrop={handleFileDrop}
+            onDragOver={(e) => e.preventDefault()}
+            onClick={() => document.getElementById('file-input').click()}
+          >
+            <FaCloudUploadAlt className="text-4xl text-blue-500 mb-2 mx-auto" />
+            <span className="text-center block">
+              Drag and drop files here, or <a href="#" className="text-blue-500">Browse</a>
+            </span>
+            <input
+              id="file-input"
+              type="file"
+              multiple
+              onChange={handleFileChange}
+              className="hidden"
+            />
+          </div>
+          {files.length > 0 && (
+            <div className="mb-4">
+              <label className="block">Selected Files:</label>
+              <ul className="w-full p-2 border border-gray-600 rounded mt-1 bg-gray-300">
+                {files.map((file, index) => (
+                  <li key={index} className="flex justify-between items-center py-1 px-2">
+                    <span>{file.name}</span>
+                    <button
+                      type="button"
+                      onClick={() => handleFileRemove(file)}
+                      className="text-red-500 hover:text-red-700"
+                    >
+                      Remove
+                    </button>
+                  </li>
+                ))}
+              </ul>
             </div>
-            <div>
-              <label className="block text-white">Date:</label>
-              <input
-                type="date"
-                readOnly
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                className="w-full p-2 border border-gray-600 rounded mt-1 bg-gray-700 text-white"
-              />
+            
+          )}
+          {message && (
+            <div className="text-red-500 text-center">
+              {message}
             </div>
-            <div>
-              <label className="block text-white">Hospital:</label>
-              <input
-                type="text"
-                value={hospital}
-                
-                 onChange={(e) => setHospital(e.target.value)}
-                placeholder="Enter hospital name"
-                className="w-full p-2 border border-gray-600 rounded mt-1 bg-gray-700 text-white"
-              />
-            </div>
-            <div>
-              <label className="block text-white">Upload File:</label>
-              <input
-                type="file"
-                onChange={handleFileChange}
-                className="w-full p-2 border border-gray-600 rounded mt-1 bg-gray-700 text-white"
-              />
-            </div>
-            {message && (
-              <div className="text-red-500 text-center">
-                {message}
-              </div>
-            )}
-            <button
-              type="button"
-              onClick={handleUpload}
-              className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
-            >
-              Upload to Blockchain
-            </button>
-          </form>
-        </div>
+          )}
+          </div>
+          <button
+            type="button"
+            onClick={handleUpload}
+            className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
+          >
+            Upload to Blockchain
+          </button>
+        </form>
       </div>
 
-      {/* Loading Popup */}
       {loading && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
           <div className="text-white text-lg">Uploading...</div>
